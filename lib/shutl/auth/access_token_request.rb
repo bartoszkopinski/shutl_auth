a34d300a86019b7ec4967dea104aac798b2cb132
Mark Burns <markthedeveloper@gmail.com>
2013-01-01 13:59:04 +0900

    wip refactoring to allow for a simpler API and also only showing Shutl::Auth specific errors

--- lib/shutl/auth/access_token_request.rb
+++ lib/shutl/auth/access_token_request.rb
@@ -1,7 +1,11 @@
 module Shutl
   module  Auth
+    class Shutl::Error       < ::StandardError; end
+    class InvalidUrl         < Shutl::Error; end
+    class InvalidCredentials < Shutl::Error; end
+
     def access_token!
-      access_token_response!.token
+      access_token_response!.access_token
     end
 
     def access_token_response!
@@ -13,22 +17,45 @@ module Shutl
     private
 
     def client
-      #TODO: handle the various exceptions that can be thrown by the OAuth2
-      #gem and turn into Shutl specific exceptions
-      Rack::OAuth2::Client.new(
+      Rack::OAuth2::Client.new \
         identifier:     Shutl::Auth.client_id,
         secret:         Shutl::Auth.client_secret,
         token_endpoint: '/token',
         host:           uri.host,
         port:           uri.port,
         scheme:         uri.scheme
-      )
+
+    rescue Rack::OAuth2::Client::Error => e
+      debugger
+      puts e.message
+      raise_invalid_credentials
+
+    rescue Exception => e
+      debugger
+      puts e.message
+
     end
 
     def uri
-      URI Shutl::Auth.url
+      check URI Shutl::Auth.url
+
+    rescue URI::InvalidURIError
+      raise_invalid_uri
     end
 
+    def check uri
+      return uri if uri and uri.host and uri.scheme
+
+      raise_invalid_uri
+    end
+
+    def raise_invalid_uri
+      raise Shutl::Auth::InvalidUrl, "Please set value of Shutl::Auth.url"
+    end
+
+    def raise_invalid_credentials
+      raise Shutl::Auth::InvalidCredentials, "Invalid credentials set, please see https://github.com/shutl/shutl_auth/blob/master/README.md"
+    end
     extend self
   end
 end
